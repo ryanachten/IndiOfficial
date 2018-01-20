3f930a1b8e89c2d7820c2434b5be4c5db43daa42
Ryan Achten <ryanachten@gmail.com>
2018-01-21 09:45:21 +1300

    Add poster fallback for lack of background video .mp4 support

--- js/indi_music_video.js
+++ js/indi_music_video.js
@@ -1,16 +1,21 @@
 $(document).ready(
 	function functionName() {
-		$('.video-container').vide('video/indi', {
-		  volume: 1,
-		  playbackRate: 1,
-		  muted: false,
-		  loop: true,
-		  autoplay: true,
-		  position: '50% 50%',
-		  posterType: 'detect',
-		  resizing: true,
-		  bgColor: 'transparent',
-		  className: ''
-		});
+		$('.video-container').vide(
+			{
+	  		mp4: 'video/indi',
+				poster: 'img/Indi_AlbumCover'
+			},{
+			  volume: 1,
+			  playbackRate: 1,
+			  muted: false,
+			  loop: true,
+			  autoplay: true,
+			  position: '50% 50%',
+			  posterType: 'detect',
+			  resizing: true,
+			  bgColor: 'transparent',
+			  className: ''
+			}
+		);
 	}
 );
